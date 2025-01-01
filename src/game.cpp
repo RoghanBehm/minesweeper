fd6d207be5033c3d6e5e261eb33d1b77eb527738
Roghan <roghanthomasbehm@gmail.com>
2025-01-01 08:08:43 +0000

    Disabled flagging when game_over is true

--- src/game.cpp
+++ src/game.cpp
@@ -197,7 +197,7 @@ void Game::createGrid(SDL_Renderer *renderer, MouseProps &mouseProps, GameAssets
             int cell_y = i * globalSettings.cell_size + globalSettings.menu_height;
 
             // Handle right-click toggle
-            if (cellClicked(mouseProps.mouseXc, mouseProps.mouseYc, cell_x, cell_y) && mouseProps.rightClicked) {
+            if (cellClicked(mouseProps.mouseXc, mouseProps.mouseYc, cell_x, cell_y) && mouseProps.rightClicked && !globalSettings.game_over) {
                 grid[i][j].isFlagged = !grid[i][j].isFlagged;
                 mouseProps.rightClicked = false;
             }
